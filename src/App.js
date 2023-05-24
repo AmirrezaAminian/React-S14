62c3839644a63dc9973ec6883fa0c14f10d47432
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-05-25 00:30:33 +0430

    Sending a Post Request

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
     setError(null);
     try {
       const response = await fetch(
-        "https://react-http-11317-default-rtdb.firebaseio.com/movies.json"
+        "https://react-http-16bdb-default-rtdb.firebaseio.com//movies.json"
       );
       if (!response.ok) {
         throw new Error('Something went wrong!');
@@ -22,15 +22,24 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      console.log(data);
+
+      const loadedMovies = [];
+
+      for(const key in data){
+        loadedMovies.push({
+          id:key,
+          title : data[key].title,
+          openingText : data[key].openingText,
+          releaseDate : data[key].releaseDate,
+
+        })
+        console.log(key);
+      }
+
+
+     
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -41,8 +50,19 @@ function App() {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-16bdb-default-rtdb.firebaseio.com/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie),
+        headers: {
+          "Content-Type": "app;ication/json",
+        },
+      }
+    ); 
+    const data = await response.json();
+     console.log(data);
   }
 
   let content = <p>Found no movies.</p>;
